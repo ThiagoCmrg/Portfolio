602d4ecbd97abc96d4712f84d497bd7495caa81e
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2024-10-16 20:32:16 -0300

    Novas features e delete de arquivos desnecessários.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Home from './containers/Home/home'
+import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Home/>
+    <CssBaseline/>
   </StrictMode>,
 )
